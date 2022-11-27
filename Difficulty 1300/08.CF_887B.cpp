14415380ee8c4866bdbe15f6a4c35457f356e608
Samia Rahman <77110531+Samia2001@users.noreply.github.com>
2022-11-27 20:17:23 +0600

    Create 08.CF_887B.cpp

---
+++ Difficulty 1300/08.CF_887B.cpp
@@ -0,0 +1,167 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define endl "\n"
+#define    rep(X,Y)     for(X=0;X<Y;X++)
+#define    rep1(X,Y)    for(X=1;X<=Y;X++)
+#define    rrep(X,Y)    for(X=Y-1;X>=0;X--)
+#define    rrep1(X,Y)   for(X=Y;X>=1;X--)
+#define ml ll T,g; cin>>T; for(g=0;g<T;g++)
+#define nml ll T = 1,g; for(g=0;g<T;g++)
+#define case "Case "<<g+1<<":"
+#define fs(n) fixed << setprecision(n)
+#define sf(T) scanf("%lld",&T)
+#define pf(T) printf("%lld\n",T)
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define Yes cout << "Yes\n"
+#define yes cout << "yes\n"
+#define No cout << "No\n"
+#define no cout << "no\n"
+#define      len(n)             (long long)((log(n)/log(10))+1.0000000001)
+#define FastRead                      \
+ios_base::sync_with_stdio(false); \
+cin.tie(0);
+#define pi acos(-1)
+#define pb push_back
+#define pll pair<ll , ll>
+#define ceill(ggg,gg) (ggg/gg)+bool(ggg%gg)
+#define pqmn priority_queue<ll, vector<ll>, std::greater<ll> >
+#define nm -1e18
+#define ok return 0
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+///Chess moves.........
+// int dx[] = {+0, +0, -1, +1}; ///Up-down, Left-Right// int dy[] = {+1, -1, +0, +0};
+// int dx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's Move// int dy[] = {-1,+1,+0,+0,+1,+1,-1,-1};
+// int dx[] = {-2, -2, -1, -1,  1,  1,  2,  2}; ///Knight's Move// int dy[] = {-1,  1, -2,  2, -2,  2, -1,  1};
+///Constants........
+const ll mod = 1e9 + 7;
+const ll Max = 2e5 + 5;
+const ll inf = 1e18 + 5;
+ll gcd(ll a, ll b)
+{
+    while (b)
+    {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+ll lcm(ll a, ll b)
+{
+    return (a / gcd(a, b) * b);
+}
+ll bigmod(ll a, ll b, ll mod)
+{
+    b %= (mod - 1);
+    ll result = 1ll;
+    while (b > 0ll)
+    {
+        if (b & 1ll) result = result * a % mod;
+        a = a * a % mod;
+        b >>= 1ll;
+    }
+    return result;
+}
+ll inverse(ll a, ll M)
+{
+    if (gcd(a, M) == 1) return bigmod(a, M - 2, M) % M;
+    return 1;
+}
+ll ncr(ll a, ll b, ll mod)
+{
+    ll x = max(a - b, b), ans = 1;
+    for (ll K = a, L = 1; K >= x + 1; K--, L++)
+    {
+        ans = ans * K % mod;
+        ans = ans * inverse(L, mod) % mod;
+    }
+    return ans;
+}
+bool cmp(const pair<ll, ll> &a, const pair<ll, ll> &b)
+{
+    if (a.first == b.first)
+        return (a.second > b.second);
+    else
+        return (a.first < b.first);
+}
+ll bs(ll n)
+{
+    ll l = 1,r = 2*sqrt(n);
+    while(r-l>1)
+    {
+        ll mid = (l+r)/2;
+        if(mid*mid==n)
+            return mid;
+        if(mid*mid<n)
+            l = mid;
+        else
+            r = mid-1;
+    }
+    if(r*r <= n)return r;
+    else return l;
+}
+int main()
+{
+
+    ll i, j, k;
+    nml
+    {
+        ll n,x=-1,a,mx=-1;
+        cin >> n;
+        map<ll, ll> mp1, mp2, mp3 ;
+        rep(j, n)
+        {
+            rep(i, 6) {
+                ll a;
+                cin >> a;
+                if(j==0)
+                    mp1[a]++ ;
+                else if(j==1)
+                    mp2[a]++ ;
+                else
+                    mp3[a]++ ;
+            }
+        }
+
+        rep1(i, 999)
+        {
+            ll p = i,c=0;
+            ll x = -1, y = -1, z = -1 ;
+            while(p)
+            {
+                if(c==0)
+                    x = p%10 ;
+                else if(c==1)
+                    y = p%10 ;
+                else
+                    z = p%10 ;
+                p/=10;
+                c++;
+            }
+
+            //cout << x << " " << y << " " << z << endl ;
+
+            if(bool(mp1[x]) + bool(mp2[y]) + bool(mp3[z]) == c)
+                    continue ;
+            else if(bool(mp1[x]) + bool(mp2[z]) + bool(mp3[y]) == c)
+                    continue ;
+            else if(bool(mp1[y]) + bool(mp2[x]) + bool(mp3[z]) == c)
+                    continue ;
+            else if(bool(mp1[y]) + bool(mp2[z]) + bool(mp3[x]) == c)
+                    continue ;
+            else if(bool(mp1[z]) + bool(mp2[x]) + bool(mp3[y]) == c)
+                    continue ;
+            else if(bool(mp1[z]) + bool(mp2[y]) + bool(mp3[x]) == c)
+                    continue ;
+            else
+                break;
+        }
+
+        cout<<i-1<<endl;
+    }
+    return 0;
+}
