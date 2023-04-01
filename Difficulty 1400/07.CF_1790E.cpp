6e84009cd0ebaea9dc9c1c3d2ac095e23b4fbc0d
Samia Rahman <77110531+Samia2001@users.noreply.github.com>
2023-04-01 10:05:19 +0600

    Create 07.CF_1790E.cpp

---
+++ Difficulty 1400/07.CF_1790E.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define    rep(X,Y)     for(X=0;X<Y;X++)
+#define    rep1(X,Y)    for(X=1;X<=Y;X++)
+#define ml ll T,g; cin>>T; for(g=0;g<T;g++)
+#define nml ll T = 1,g; for(g=0;g<T;g++)
+#define pb push_back
+#define pll pair<ll , ll>
+using namespace std;
+int main()
+{
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("outp1.txt", "w", stdout);
+#endif
+  ll i, j;
+  ml {
+  ll n;
+  cin >> n;
+  if(n%2)cout<<-1<<endl;
+  else
+  {
+    ll a = n+(n/2);
+    ll b = n/2;
+    if((a^b)!=n)cout<<-1<<endl;
+    else
+    cout<<a<<" "<<b<<endl;
+  }
+
+  }
+
+  return 0;
+}
